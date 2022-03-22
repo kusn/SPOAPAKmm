51d4d42ae85fea9382fce6f787d9884a29233381
Kusn <serjikf@yandex.ru>
2022-03-22 15:41:24 +0300

    Entity "Organization"

---
+++ SPOAPAKmmReceiver/Models/Organization.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SPOAPAKmmReceiver.Models.Base;
+
+namespace SPOAPAKmmReceiver.Models
+{
+    public class Organization : Entity
+    {
+        [NotNull]
+        public string Name { get; set; }
+
+        public string? Address { get; set; }
+
+        public ICollection<Room> Rooms { get; set; }
+    }
+}
