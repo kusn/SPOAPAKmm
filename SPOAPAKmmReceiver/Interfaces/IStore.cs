40d270dc96ab6a787ea56f035f9da165f04a5847
Kusn <serjikf@yandex.ru>
2022-03-23 09:53:30 +0300

    Create IStore

---
+++ SPOAPAKmmReceiver/Interfaces/IStore.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SPOAPAKmmReceiver.Models.Base;
+
+namespace SPOAPAKmmReceiver.Interfaces
+{
+    public interface IStore<T> where T : Entity
+    {
+        IEnumerable<T> GetAll();
+        
+        T GetById(int id);
+
+        T Add(T item);
+
+        T Update(T item);
+
+        void Delete(T item);
+    }
+}
