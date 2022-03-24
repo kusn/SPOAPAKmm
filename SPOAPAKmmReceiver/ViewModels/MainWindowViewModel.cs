63a92d09393018cbc5da5989b6d1c628a4c95dd9
Kusn <serjikf@yandex.ru>
2022-03-24 15:39:09 +0300

    Blank ContentControl

--- SPOAPAKmmReceiver/ViewModels/MainWindowViewModel.cs
+++ SPOAPAKmmReceiver/ViewModels/MainWindowViewModel.cs
@@ -1,14 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using SPOAPAKmmReceiver.Interfaces;
+using SPOAPAKmmReceiver.Models;
 using SPOAPAKmmReceiver.ViewModels.Base;
 
 namespace SPOAPAKmmReceiver.ViewModels
 {
     public class MainWindowViewModel : ViewModel
     {
+        private readonly IStore<Organization> _dbOrganizationsStore;
+
+        public ObservableCollection<Organization> Organizations { get; set; }
+
+        public ObservableCollection<Room> Rooms { get; set; }
+
+        public ObservableCollection<Element> Elements { get; set; }
+
+        public ObservableCollection<Device> Devices { get; set; }
+
+        public ObservableCollection<MeasPoint> MeasPoints { get; set; }
+
+        public ObservableCollection<Measuring> Measurings { get; set; }
+
+        public Page UserPage { get; set; }
 
     }
 }
