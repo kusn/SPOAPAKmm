27f3694b053cc4b2acc187c424cc415ac1a7646f
Kusn <serjikf@yandex.ru>
2022-06-06 13:27:12 +0300

    Base of SPOAPAKmmTransmitter

--- SPOAPAKmmTransmitter/Program.cs
+++ SPOAPAKmmTransmitter/Program.cs
@@ -5,14 +5,19 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text.Json;
 using System.Threading;
+using static SPOAPAKmmReceiver.Models.ReceiverMessage;
 
 namespace SPOAPAKmmTransmitter
 {
     internal class Program
     {
+        static private string _ipAddress = "127.0.0.1";
+        static private int _listenerPort = 11000;
+        static private int _sendPort = 11001;
+
         private static void GetAccessToClientProgram()
         {
-            TcpListener listner = new TcpListener(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 11000));
+            TcpListener listner = new TcpListener(new IPEndPoint(IPAddress.Parse(_ipAddress), _listenerPort));
             listner.Start();
             while (true)
             {
@@ -24,10 +29,26 @@ namespace SPOAPAKmmTransmitter
 
                 if (m != null)
                 {
-                    if (m.Mode == ReceiverMessage.WorkMode.ApplyMeasureSettings)
+                    if(m.Mode == WorkMode.Searching)
+                    {
+
+                    }
+                    else if (m.Mode == WorkMode.ApplyInstrumentSettings)
+                    {
+
+                    }
+                    else if (m.Mode == WorkMode.ApplyMeasureSettings)
                     {
                         Console.WriteLine("Получены настройки: " + s);
                         SendToClient("Настройки приняты: " + DateTime.Now.TimeOfDay.ToString());
+                    }
+                    else if (m.Mode == WorkMode.Сalibration)
+                    {
+
+                    }
+                    else if (m.Mode == WorkMode.Measuring)
+                    {
+
                     }
                 }
 
@@ -45,7 +66,7 @@ namespace SPOAPAKmmTransmitter
             TcpClient client = new TcpClient();
             try
             {
-                client.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 11001));
+                client.Connect(new IPEndPoint(IPAddress.Parse(_ipAddress), _sendPort));
                 StreamWriter sw = new StreamWriter(client.GetStream());
                 sw.AutoFlush = true;
                 sw.WriteLine(Massage);
