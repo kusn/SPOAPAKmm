9cf952dc524a62e999eca1a3478d3c006d2daed6
Kusn <serjikf@yandex.ru>
2022-06-16 14:19:26 +0300

    mapper InstrumentSettingsFromStringArray not use

---
+++ SPOAPAKmmReceiver/Extensions/DTO/Mapper.cs
@@ -0,0 +1,17 @@
+﻿using SPOAPAKmmReceiver.Models;
+using System;
+
+namespace SPOAPAKmmReceiver.Extensions.DTO
+{
+    public static class Mapper
+    {
+        //public static InstrumentSettings InstrumentSettingsFromStringArray(this string[] item) => item is null
+        //    ? null
+        //    : new InstrumentSettings
+        //    {
+        //        InstrAddress = item[0],
+        //        IpAddress = System.Net.IPAddress.Parse(item[1]),
+        //        Port = Convert.ToInt32(item[2]),
+        //    };
+    }
+}
