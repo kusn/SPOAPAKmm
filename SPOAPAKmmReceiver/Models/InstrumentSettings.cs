57dd86d523fb3e3a2f03e3f1ad100892a34e9801
Kusn <serjikf@yandex.ru>
2022-06-16 14:21:27 +0300

    refactoring InstrumentSettings (add extra fields)

--- SPOAPAKmmReceiver/Models/InstrumentSettings.cs
+++ SPOAPAKmmReceiver/Models/InstrumentSettings.cs
@@ -1,17 +1,23 @@
 ﻿using SPOAPAKmmReceiver.Models.Base;
 using System;
+using System.ComponentModel;
 using System.Net;
 
 namespace SPOAPAKmmReceiver.Models
 {
     [Serializable()]
-    public class InstrumentSettings : Model
+    public class InstrumentSettings : Model, IDataErrorInfo
     {
-        private IPAddress _ipAddress;
+        private string _ipAddress;
         private int _port;
         private string _instrAddress;
 
-        public IPAddress IpAddress
+        [field: NonSerialized()]
+        public const string Generator = "Generator";
+        [field: NonSerialized()]
+        public const string Receiver = "Receiver";
+
+        public string IpAddress
         { 
             get => _ipAddress;
             set => Set(ref _ipAddress, value);
@@ -26,6 +32,35 @@ namespace SPOAPAKmmReceiver.Models
         {
             get => _port;
             set => Set(ref _port, value);
+        }
+
+        [field: NonSerialized()]
+        public string Error => String.Empty;
+        
+        [field: NonSerialized()]
+        public string this[string columnName]
+        {
+            get
+            {
+                string error = String.Empty;
+                switch (columnName)
+                {
+                    case "IpAddress":
+                        IPAddress address;
+                        if (!IPAddress.TryParse(IpAddress, out address))
+                            error = "Неверный формат IP адреса";
+                        break;
+
+                    case "InstrAddress":
+                        break;
+
+                    case "Port":
+                        if (Port <= 0)
+                            error = "Неверное значение порта";
+                        break;
+                }
+                return error;
+            }
         }
     }
 }
