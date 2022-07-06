8a4bd2b666475a1a0bd9a1984efd13067c7ba0cb
Kusn <serjikf@yandex.ru>
2022-07-06 10:22:49 +0300

    Testing of RS Instrument's libraries

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -1,75 +1,133 @@
-﻿using SPOAPAKmmReceiver.Models;
+﻿using RohdeSchwarz.RsFsw;
+//using RohdeSchwarz.RsSmab;
 using System;
-using System.IO;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
-using System.Text.Json;
-using System.Threading;
+using System.Collections.Generic;
+using TestConsoleApp.RS;
 
 namespace TestConsoleApp
 {
     internal class Program
-    {
-        /// <summary>
-        /// Подключается к приложению-клиенту и выполняет прослушивание его сообщений
-        /// </summary>
-        private static void GetAccessToClientProgram()
-        {
-            TcpListener listner = new TcpListener(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 11000));
-            listner.Start();
-            while (true)
-            {
-                TcpClient client = listner.AcceptTcpClient();
-                StreamReader sr = new StreamReader(client.GetStream());
-
-                var s = sr.ReadLine();
-                var m = JsonSerializer.Deserialize<ReceiverMessage>(s);
-
-                if (m != null)
-                {
-                    if (m.Mode == ReceiverMessage.WorkMode.ApplyMeasureSettings)
-                    {
-                        Console.WriteLine("Получены настройки: " + s);
-                        SendToClient("Настройки приняты " + DateTime.Now.TimeOfDay.ToString());
-                    }
-                }
-
-                //Console.WriteLine("Сообщение: " + s + " получено в " + DateTime.Now.TimeOfDay.ToString());
-                //Execute(sr.ReadLine());   //<---------- самописная функция Execute, что-то выполняет с пришедшими данными
-                //Thread.Sleep(1000);
-                //SendToClient("Сообщение отправлено сервером в " + DateTime.Now.TimeOfDay.ToString());
-
-                client.Close();
-            }
-        }
-
-        /// <summary>
-        /// Посылает сообщение приложению-клиенту
-        /// </summary>
-        /// <param name="Massage">Передаваемое сообщение</param>
-        private static void SendToClient(string Massage)
-        {
-            TcpClient client = new TcpClient();
-            try
-            {
-                client.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 11001));
-                StreamWriter sw = new StreamWriter(client.GetStream());
-                sw.AutoFlush = true;
-                sw.WriteLine(Massage);
-            }
-            catch
-            {
-                Console.WriteLine("Ошибка при подключении к Client.exe");
-            }
-            client.Close();
-        }
-
+    { 
         public static void Main(String[] args)
         {
-            Thread AccessToClientProgram = new Thread(GetAccessToClientProgram);
-            //AccessToClientProgram.IsBackground = true;
-            AccessToClientProgram.Start();
+            //StartSpecAn();
+            StartSignalGenerator();
+        }
+
+        private static void StartSpecAn()
+        {
+            var fsw = new RsFsw("GPIB0::20::INSTR", true, true);
+            //var fsw = new RsFsw("TCPIP::192.168.1.102::INSTR", true, true, "SelectVisa=RsVisa"); // Forcing R&S VISA
+            //var fsw = new RsFsw("TCPIP::192.168.1.102::5025::SOCKET", true, true, "SelectVisa=SocketIo"); // No VISA installation needed
+            Console.WriteLine("Driver Info: " + fsw.Utilities.Identification.DriverVersion);
+            Console.WriteLine("Instrument: " + fsw.Utilities.Identification.IdnString);
+            Console.WriteLine("Instrument options: " + string.Join(",", fsw.Utilities.Identification.InstrumentOptions));
+
+            // Driver's instrument status checking ( SYST:ERR? ) after each command (default value is true):
+            fsw.Utilities.InstrumentStatusChecking = true;
+
+            //  SYSTem:DISPlay:UPDate ON
+            fsw.System.Display.Update.Set(true);
+
+            //  INITiate:CONTinuous OFF
+            fsw.Initiate.Continuous.Set(false);
+            Console.WriteLine("Always work in single-sweep mode.");
+
+            //  SENSe.FREQuency:STARt 100000000
+            fsw.Sense.Frequency.Start.Set(100E6);
+
+            //  SENSe.FREQuency:STOP 200000000
+            fsw.Sense.Frequency.Stop.Set(200E6);
+
+            //  DISPlay:WINDow:TRACe:Y:SCALe:RLEVel -20.0
+            fsw.Display.Window.Trace.Y.Scale.RefLevel.Set(-20.0);
+
+            //  DISPlay:WINDow:SUBWindow:TRACe:Y:SCALe 60.0
+            fsw.Display.Window.Subwindow.Trace.Y.Scale.Set(60.0);
+
+            //  DISPlay1:WINDow:SUBWindow:TRACe1:MODE MAXHold
+            fsw.Display.Window.Subwindow.Trace.Mode.Set(TraceModeCenum.MAXHold, WindowRepCap.Nr1, SubWindowRepCap.Default, RohdeSchwarz.RsFsw.TraceRepCap.Tr1);
+
+            //  DISPlay1:WINDow:SUBWindow:TRACe2:MODE MINHold
+            fsw.Display.Window.Subwindow.Trace.Mode.Set(TraceModeCenum.MINHold, WindowRepCap.Nr1, SubWindowRepCap.Default, RohdeSchwarz.RsFsw.TraceRepCap.Tr2);
+
+            //  SENSe:SWEep:POINts 10001
+            fsw.Sense.Sweep.Points.Set(10001);
+
+            //  INITiate:IMMediate (with timeout 3000 ms)
+            fsw.Initiate.ImmediateAndWait(3000);
+
+            //                        TRACe1:DATA?
+            List<double> trace1 = fsw.Trace.Data.Get(TraceNumberEnum.TRACe1);
+
+            //                        TRACe2:DATA?
+            List<double> trace2 = fsw.Trace.Data.Get(TraceNumberEnum.TRACe2);
+
+            //  CALCulate1:MARKer1:TRACe 1
+            fsw.Calculate.Marker.Trace.Set(1, WindowRepCap.Nr1, RohdeSchwarz.RsFsw.MarkerRepCap.Nr1);
+
+            //  CALCulate1:MARKer1:MAXimum:PEAK
+            fsw.Calculate.Marker.Maximum.Peak.Set(WindowRepCap.Nr1, RohdeSchwarz.RsFsw.MarkerRepCap.Nr1);
+            //               CALCulate1:MARKer1:X?
+            double m1x = fsw.Calculate.Marker.X.Get(WindowRepCap.Nr1, RohdeSchwarz.RsFsw.MarkerRepCap.Nr1);
+
+            //               CALCulate1:MARKer1:Y?
+            double m1y = fsw.Calculate.Marker.Y.Get(WindowRepCap.Nr1, RohdeSchwarz.RsFsw.MarkerRepCap.Nr1);
+
+            Console.WriteLine($"Trace 1 points: {trace1.Count}");
+            Console.WriteLine($"Trace 1 Marker 1: {m1x} Hz, {m1y:F2} dBm");
+
+            //  CALCulate1:MARKer2:TRACe 2
+            fsw.Calculate.Marker.Trace.Set(2, WindowRepCap.Nr1, RohdeSchwarz.RsFsw.MarkerRepCap.Nr2);
+
+            //  CALCulate1:MARKer2:MINimum:PEAK
+            fsw.Calculate.Marker.Minimum.Peak.Set(WindowRepCap.Nr1, RohdeSchwarz.RsFsw.MarkerRepCap.Nr2);
+
+            //               CALCulate2:MARKer2:X?
+            double m2x = fsw.Calculate.Marker.X.Get(WindowRepCap.Nr1, RohdeSchwarz.RsFsw.MarkerRepCap.Nr2);
+
+            //               CALCulate2:MARKer2:Y?
+            double m2y = fsw.Calculate.Marker.Y.Get(WindowRepCap.Nr1, RohdeSchwarz.RsFsw.MarkerRepCap.Nr2);
+
+            Console.WriteLine($"Trace 2 points: {trace2.Count}");
+            Console.WriteLine($"Trace 2 Marker 2: {m2x} Hz, {m2y:F2} dBm");
+
+            // Close the session
+            fsw.Dispose();
+
+            Console.WriteLine("\nPress any key");
+            Console.ReadKey();
+        }
+
+        private static void StartSignalGenerator()
+        {
+            var smab = new RsSmab("GPIB1::28::INSTR", true, true);
+            //var smab = new RsSmab("TCPIP::10.112.1.73::INSTR", true, true, "SelectVisa=RsVisa"); // Forcing R&S VISA
+            //var smab = new RsSmab("TCPIP::10.112.1.73::5025::SOCKET", true, true, "SelectVisa=SocketIo"); // No VISA needed
+            Console.WriteLine("Driver Info: " + smab.Utilities.Identification.DriverVersion);
+            Console.WriteLine($"Selected Visa: {smab.Utilities.Identification.VisaManufacturer}, DLL: {smab.Utilities.Identification.VisaDllName}");
+            Console.WriteLine("Instrument: " + smab.Utilities.Identification.IdnString);
+            Console.WriteLine("Instrument options: " + string.Join(",", smab.Utilities.Identification.InstrumentOptions));
+
+            // Driver's instrument status checking ( SYST:ERR? ) after each command (default value is true):
+            smab.Utilities.InstrumentStatusChecking = true;
+            smab.Utilities.Reset();
+
+            // Set the output -21.3 dBm, 224 MHz
+            smab.Output.State.Value = true;
+            smab.Source.Power.Level.Immediate.Amplitude = -21.3;
+            smab.Source.Frequency.Fixed.Value = 234E6;
+            smab.Output.State.Value = false;
+
+            // Direct SCPI interface:
+            var response = smab.Utilities.QueryString("*IDN?");
+            Console.WriteLine($"Direct SCPI response on *IDN?: {response}");
+
+            // Closing the session
+            smab.Dispose();
+
+            Console.Write("\n\nPress any key...");
+            Console.ReadKey();
         }
     }
 }
