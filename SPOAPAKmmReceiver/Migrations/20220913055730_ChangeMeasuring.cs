1286881b23092f429d7cbcf36e66ca81a3966be6
Kusn <serjikf@yandex.ru>
2022-09-13 09:00:04 +0300

    Add migration and update database

---
+++ SPOAPAKmmReceiver/Migrations/20220913055730_ChangeMeasuring.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SPOAPAKmmReceiver.Migrations
+{
+    public partial class ChangeMeasuring : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "Organizations",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AddColumn<double>(
+                name: "AverageE",
+                table: "Measurings",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "DX",
+                table: "Measurings",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "E",
+                table: "Measurings",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AverageE",
+                table: "Measurings");
+
+            migrationBuilder.DropColumn(
+                name: "DX",
+                table: "Measurings");
+
+            migrationBuilder.DropColumn(
+                name: "E",
+                table: "Measurings");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "Organizations",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
